aa6a5dcac6c1771be28e5f414d89ade5fea14b75
treethought <cpsweene@gmail.com>
2021-05-27 09:13:09 -0700

    initial work to encrypt

--- crypto.go
+++ crypto.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha1"
 	"encoding/base64"
 	"encoding/hex"
@@ -23,6 +24,46 @@ func genAesKey(accountId string, pwHash string, salt []byte) []byte {
 	combinedKey := fmt.Sprintf("%s:%s\n", accountId, pwHash)
 	key := pbkdf2.Key([]byte(combinedKey), salt, 10000, 32, sha1.New)
 	return key
+
+}
+
+func randomHex(length int) (string, error) {
+	n := length / 2
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(b), nil
+
+}
+
+
+func encrypt(block cipher.Block, data string) (string, error) {
+	rhex, err := randomHex(128)
+	if err != nil {
+		return "", err
+	}
+
+	hexString, _ := hex.DecodeString(rhex)
+	iv := hexString[:block.BlockSize()]
+
+	if len(iv) != block.BlockSize() {
+		err := fmt.Errorf("iv length %v does not equal block size %v", len(iv), block.BlockSize())
+		panic(err)
+	}
+
+	plain := []byte(data)
+
+	plain, _ = pkcs7Pad(plain, block.BlockSize())
+
+	if len(plain)%block.BlockSize() != 0 {
+		err := fmt.Errorf("ciphertext of len %v is not a multiple of the block size: %v", len(plain), block.BlockSize())
+		panic(err)
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(plain, plain)
+	return base64.StdEncoding.EncodeToString(iv) + "-:-" + base64.StdEncoding.EncodeToString(plain), nil
 
 }
 
@@ -56,6 +97,24 @@ func decrypt(block cipher.Block, data string) string {
 
 }
 
+// pkcs7Pad right-pads the given byte slice with 1 to n bytes, where
+// n is the block size. The size of the result is x times n, where x
+// is at least 1.
+// https://gist.github.com/huyinghuan/7bf174017bf54efb91ece04a48589b22
+func pkcs7Pad(b []byte, blocksize int) ([]byte, error) {
+	if blocksize <= 0 {
+		return nil, fmt.Errorf("invalid block size")
+	}
+	if b == nil || len(b) == 0 {
+		return nil, fmt.Errorf("invalid pkcs7 data format")
+
+	}
+	n := blocksize - (len(b) % blocksize)
+	pb := make([]byte, len(b)+n)
+	copy(pb, b)
+	copy(pb[len(b):], bytes.Repeat([]byte{byte(n)}, n))
+	return pb, nil
+}
 func decryptConversation(block cipher.Block, convo *Conversation) (err error) {
 
 	// Removes miliiseconds from timestamp
